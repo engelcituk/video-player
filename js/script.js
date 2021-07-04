3a6ee9dd2548890e716800bbfc99eaf1d408f97d
cituk <programaciondyn3.mx@sandos.com>
2021-07-04 04:43:01 -0500

    fullscreen

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ const pause = document.querySelector('#pause')
 const backward = document.querySelector('#backward')
 const forward = document.querySelector('#forward')
 const progress = document.querySelector('#progress')
+const fullscreen = document.querySelector('#fullscreen')
 
 play.addEventListener('click', handlePlay)
 pause.addEventListener('click', handlePause)
 backward.addEventListener('click', handleBackward)
 forward.addEventListener('click', handleForward)
+fullscreen.addEventListener('click', handleFullscreen)
 video.addEventListener('loadedmetadata', handleLoaded)
 video.addEventListener('timeupdate', handleTimeUpdate)
 progress.addEventListener('input', handleInput)
@@ -25,10 +27,15 @@ function handlePause() {
     play.hidden = false
     pause.hidden = true
 }
-
-
 function handleForward() {
     video.currentTime += 10
+}
+
+function handleFullscreen() {
+    video.requestFullscreen()
+    video.mozRequestFullScreen()
+    video.webkitRequestFullscreen()
+    video.msRequestFullscreen()
 }
 
 function handleBackward() {
@@ -41,8 +48,12 @@ function handleLoaded() {
 
 function handleTimeUpdate() {
     progress.value = video.currentTime 
+    fullscreen.hidden = false
 }
 
 function handleInput() {
+    if( progress.value > 0){
+        video.play()
+    }
     video.currentTime = progress.value 
 }
